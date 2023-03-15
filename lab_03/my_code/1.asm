ec14d76c37562d45dda9cd6ab5c9d230963a8b54
31steets31 <stepaha78@gmail.com>
2023-03-15 12:30:21 +0300

    Adding lab_03

---
+++ lab_03/my_code/1.asm
@@ -0,0 +1,39 @@
+EXTRN PrintHexNumber: near
+PUBLIC second_number
+
+DSEG SEGMENT PARA PUBLIC 'DATA'
+INPUT_MSG db 13
+    db 10
+    db "Input number (10 - 15): $"
+first_number db ?
+second_number db ?
+DSEG ENDS
+
+STK SEGMENT PARA STACK 'STACK'
+	db 20 dup(0)
+STK ENDS
+
+CSEG SEGMENT PARA PUBLIC 'CODE'
+    assume CS:CSEG, DS:DSEG, SS:STK
+main:
+    mov AX, DSEG
+    mov DS, AX
+print_input_msg:
+    mov AH, 9h
+    mov DX, OFFSET INPUT_MSG
+    int 21h
+read_data:
+    mov AH, 1h
+    int 21h
+    mov first_number, AL
+
+    int 21h
+    mov second_number, AL
+print_result:
+    call PrintHexNumber
+
+    mov AH, 4Ch
+    int 21h
+CSEG ENDS
+
+END main
---
+++ lab_03/my_code/2.asm
@@ -0,0 +1,30 @@
+EXTRN second_number: byte
+PUBLIC PrintHexNumber
+
+DSEG SEGMENT PARA PUBLIC 'DATA'
+OUTPUT_MSG db 13
+    db 10
+    db "Converted to HEX: $"
+DSEG ENDS
+
+CSEG SEGMENT PARA PUBLIC 'CODE'
+    assume CS:CSEG, DS:DSEG
+PrintHexNumber proc near
+    mov AX, DSEG
+    mov DS, AX
+
+    mov AL, second_number
+    add AL, 17
+
+    mov AH, 9h
+    mov DX, OFFSET OUTPUT_MSG
+    int 21h
+
+    mov AH, 2h
+    mov DL, AL
+    int 21h
+
+    ret
+PrintHexNumber endp
+CSEG ENDS
+END
